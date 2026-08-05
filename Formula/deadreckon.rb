75009d315be8602db4868040417289ae50e21aac
Greg Ceccarelli <gregce@gmail.com>
2026-08-05 04:00:39 -0400

    deadreckon 0.8.1: pin archive checksums

--- Formula/deadreckon.rb
+++ Formula/deadreckon.rb
@@ -9,17 +9,21 @@ class Deadreckon < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/gregce/deadreckon/releases/download/v0.8.1/deadreckon-aarch64-apple-darwin.tar.xz"
+      sha256 "9587a9cf9ae9205091c2f7378e31ff1a91bdf9e7b52c5f8224bd586a2e437d63"
     end
     if Hardware::CPU.intel?
       url "https://github.com/gregce/deadreckon/releases/download/v0.8.1/deadreckon-x86_64-apple-darwin.tar.xz"
+      sha256 "f0f00b1b5bdd6ddb12495779991eca82450fa22c065126837d75ef7916e9e718"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/gregce/deadreckon/releases/download/v0.8.1/deadreckon-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "392e6703f368d671692c4b2ae0a963dcf1f1e72776f0b7ea6a1e3ceb03b95eaf"
     end
     if Hardware::CPU.intel?
       url "https://github.com/gregce/deadreckon/releases/download/v0.8.1/deadreckon-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "4ccdf8eb7511ac5f8b8c62d8dd3b73ddb18e40f5a9b0044fca7ed6c149255647"
     end
   end
   license "MIT"
